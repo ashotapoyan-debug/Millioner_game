a6bc167a1e320aee59a604c9525b63cbc706c18b
ashotapoyan-debug <anushik.papikyan@tumo.org>
2026-07-24 20:11:38 +0400

    css and js chages

---
+++ js/script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+let meinGame = document.querySelector('.game-block'),
+gameWrapper = document.querySelector('.game-wrap'),
+starBtn = document.querySelector('.start-btn'),
+endBtn = document.querySelector('.end-btn'),
+btnAnswers = document.querySelectorAll('.answer'),
+blockQuestion = document.querySelectorAll('.question'),
+helpBtns = document.querySelectorAll('.hints-help'),
+winBlock = document.querySelectorAll('.wins-block'),
+helpFiffy = document.querySelector('.fifty-fifty'),
+helpHall = document.querySelector('.help-help'),
+helpFriend = document.querySelector('.call-friend'),
+helpAI = document.querySelector('.ai-help'),
+game = document.querySelector('.game')
+////////////////////=========================>
+
+let changeQuestion = document.getElementById('hitBox')
+let extraQuestion = document.getElementById('extra')
+let flagExtra = true
